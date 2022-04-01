8b2a83d24f43caeaf8f79d37d9194e465d8ad3fb
ltbur <ltburtonst@gmail.com>
2022-04-01 14:22:29 +0900

    Odai-1追加

--- Abe/Q001.java
+++ Abe/Q001.java
@@ -12,5 +12,10 @@
 import java.util.*;
 public class Q001{
 	public static void main(String[] args){
+		System.out.print("整数1>>");
+		int n1 = new Scanner(System.in).nextInt();
+		System.out.print("整数2>>");
+		int n2 = new Scanner(System.in).nextInt();
+		System.out.printf("%dと%dの合計は%dです。%n",n1,n2,n1+n2);
 	}
 }
