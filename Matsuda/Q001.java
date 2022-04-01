c79965f27db7ab755bfccb1ec4764ed37f0217b7
mjpurin <mjpurin@gmail.com>
2022-04-01 14:08:52 +0900

    Q001追加

--- Matsuda/Q001.java
+++ Matsuda/Q001.java
@@ -12,5 +12,10 @@
 import java.util.*;
 public class Q001{
 	public static void main(String[] args){
+		System.out.print("整数1 >>");
+		int n1=new Scanner(System.in).nextInt();
+		System.out.print("整数2 >>");
+		int n2=new Scanner(System.in).nextInt();
+		System.out.printf("%d と %d の合計は %d です。%n",n1,n2,n1+n2);
 	}
 }
