54e210c1b0abd2ac7faeca4dfe3d0675a08b2dfe
yakan <yume_drop@hotmail.com>
2022-04-01 16:08:17 +0900

    Q002追加

--- Nagano/Q002.java
+++ Nagano/Q002.java
@@ -10,5 +10,13 @@
 import java.util.*;
 public class Q002{
 	public static void main(String[] args){
+		System.out.print("正の整数を入力してください>");
+		int num =new Scanner(System.in).nextInt();
+		int[] nums = new int[num];
+		int sum = 0;
+		for(int i =0;i<num;i++){
+			sum+=i+1;
+		}
+		System.out.println("1から "+ num +"までの合計は"+ sum + "です");
 	}
 }
