1e8117c57d4679a8661542c4f746f44bc4383ec8
arukreid <sasaki3y98521j@gmail.com>
2022-04-01 14:10:45 +0900

    フォルダ名変更 Q001 終了

--- sasaki/Q001.java
+++ sasaki/Q001.java
@@ -12,5 +12,11 @@
 import java.util.*;
 public class Q001{
 	public static void main(String[] args){
+		System.out.println("2つの整数を入力してください。");
+		System.out.print("整数1>>");
+		int num1 = new Scanner(System.in).nextInt();
+		System.out.print("整数2>>");
+		int num2 = new Scanner(System.in).nextInt();
+		System.out.printf("%dと%dの合計は%dです。%n",num1,num2,num1+num2);
 	}
 }
