41e47f8ae3bf4dad93fdd5063d1a574d8a3456c3
sasakiy916 <sasaki3y98521j@gmail.com>
2022-04-01 23:23:24 +0900

    Q009 完了

--- sasaki/Q009.java
+++ sasaki/Q009.java
@@ -12,5 +12,48 @@
 import java.util.*;
 public class Q009{
 	public static void main(String[] args){
+		System.out.print("整数1を入力>>");
+		int num1 = new Scanner(System.in).nextInt();
+		System.out.print("整数2を入力>>");
+		int num2 = new Scanner(System.in).nextInt();
+		System.out.print("偶数(0)・奇数(1)を選択>>");
+		int select = new Scanner(System.in).nextInt();
+		int sum = 0;
+		if(select == 0){
+			if(num1 < num2){
+				System.out.printf("%dから%dまでの偶数の合計は",num1,num2);
+				for(int i=num1;i<=num2;i++){
+					if(i%2==0){
+						sum += i;
+					}
+				}
+			}else{
+				System.out.printf("%dから%dまでの偶数の合計は",num2,num1);
+				for(int i=num2;i<=num1;i++){
+					if(i%2==0){
+						sum += i;
+					}
+				}
+			}
+		}else if(select == 1){
+			if(num1 < num2){
+				System.out.printf("%dから%dまでの奇数の合計は",num1,num2);
+				for(int i=num1;i<=num2;i++){
+					if(i%2!=0){
+						sum += i;
+					}
+				}
+			}else{
+				System.out.printf("%dから%dまでの奇数の合計は",num2,num1);
+				for(int i=num2;i<=num1;i++){
+					if(i%2!=0){
+						sum += i;
+					}
+				}
+			}
+		}else{
+			System.out.println("偶数か奇数を選んで下さい。");
+		}
+		System.out.printf("%dです.",sum);
 	}
 }
