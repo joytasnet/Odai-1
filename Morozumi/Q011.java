7038556d96522e2da84d66c80c6f80ce4567e2b5
azerty <morozumiyumie@gmail.com>
2022-04-01 14:02:57 +0900

    Odai

---
+++ Morozumi/Q011.java
@@ -0,0 +1,13 @@
+/*
+【問題11】
+下の図のように、整数を入力すると、その整数が、0以上10以下であるかを判定する。
+
+【実行画面】
+整数を入力>>5
+5は0以上10以下です。
+*/
+import java.util.*;
+public class Q011{
+	public static void main(String[] args){
+	}
+}
---
+++ Morozumi/Q020.java
@@ -0,0 +1,16 @@
+/*
+【問題20】
+名前をカンマ区切りで入力。
+それを名前の文字数の多い順に表示する。
+【実行例】
+名前をカンマ区切りで入力>>John,Paul,Ringo,George
+George
+Ringo
+John
+Paul
+*/
+import java.util.*;
+public class Q020{
+	public static void main(String[] args){
+	}
+}
