5367e60048bbbe383678f4845f0aa1f6de6451e2
yakan <yume_drop@hotmail.com>
2022-04-01 16:49:08 +0900

    Q003追加

--- Nagano/Q003.java
+++ Nagano/Q003.java
@@ -13,5 +13,21 @@
 import java.util.*;
 public class Q003{
 	public static void main(String[] args){
+		System.out.print("整数1を入力>");
+		int a =new Scanner(System.in).nextInt();
+		System.out.print("整数2を入力>");
+		int b =new Scanner(System.in).nextInt();
+		System.out.print("整数3を入力>");
+		int c =new Scanner(System.in).nextInt();
+		int[] num = {a,b,c};
+		int max =num[0];
+		int min =num[0];
+		for(int i =0;i<num.length;i++){
+			max=Math.max(max,num[i]);
+			min=Math.min(min,num[i]);
+		}
+		System.out.println("最も大きい整数"+ max +"と");
+		System.out.println("最も小さい整数"+ min +"との差は"+ (max-min) +"です");
+
 	}
 }
