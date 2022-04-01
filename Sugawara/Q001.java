c709c79db1edc83b47f96625cbaca497f883f39f
pommebarque-megumi <cache2gecko@gmail.com>
2022-04-01 14:11:00 +0900

    Q001終了

--- Sugawara/Q001.java
+++ Sugawara/Q001.java
@@ -12,5 +12,12 @@
 import java.util.*;
 public class Q001{
 	public static void main(String[] args){
+		System.out.print("整数1>>");
+		int num1 = new Scanner(System.in).nextInt();
+		System.out.print("整数1>>");
+		int num2 = new Scanner(System.in).nextInt();
+
+		int sum = num1 + num2;
+		System.out.printf("%dと%dの合計は%dです。%n",num1,num2,sum);
 	}
 }
