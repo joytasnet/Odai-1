acba24bbf6ab1175bbfa71d7fd8cb5e42eea5301
pommebarque-megumi <cache2gecko@gmail.com>
2022-04-01 23:50:17 +0900

    Q4,5+

--- Sugawara/Q005.java
+++ Sugawara/Q005.java
@@ -9,5 +9,14 @@
 import java.util.*;
 public class Q005{
 	public static void main(String[] args){
+		System.out.print("整数を入力してください>>");
+		int n = new Scanner(System.in).nextInt();
+		int[] nums = new int[n];
+
+		for(int i=0 ; i<nums.length ; i++){
+			nums[i] = i+1;
+		}
+
+		System.out.println(Arrays.toString(nums));
 	}
 }
