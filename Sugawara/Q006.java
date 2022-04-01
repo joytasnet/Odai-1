6b0093f585748e152630c74a474638c9ff2d42b4
pommebarque-megumi <cache2gecko@gmail.com>
2022-04-02 00:26:50 +0900

    Q6+

--- Sugawara/Q006.java
+++ Sugawara/Q006.java
@@ -18,5 +18,25 @@
 import java.util.*;
 public class Q006{
 	public static void main(String[] args){
+		System.out.print("先頭の整数を入力>>");
+		int start = new Scanner(System.in).nextInt();
+		System.out.print("末尾の整数を入力>>");
+		int end = new Scanner(System.in).nextInt();
+		int sum = Math.max(start,end) - Math.min(start,end)+1;
+		if(sum<0){
+			sum*=(-1);
+		}
+
+		int[] numbers = new int[sum];
+		if(end<0){
+			for(int i=0 ; i<numbers.length ; i++){
+				numbers[i]=start-i;
+			}
+		}else{
+			for(int i=0 ; i<numbers.length ; i++){
+				numbers[i]=start+i;
+			}
+		}
+		System.out.println(Arrays.toString(numbers));
 	}
 }
