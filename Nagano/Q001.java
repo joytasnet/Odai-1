fc0b441f0c05753f1460589e01b9336665c88dd1
yakan <yume_drop@hotmail.com>
2022-04-01 14:15:55 +0900

    Q001追加

--- Nagano/Q001.java
+++ Nagano/Q001.java
@@ -12,12 +12,12 @@
 import java.util.*;
 public class Q001{
 	public static void main(String[] args){
-	}
+	
 	System.out.println("２つの整数を入力してください。");
 	System.out.print("整数1>");
 	int one =new Scanner(System.in).nextInt();
 	System.out.print("整数2>");
 	int two =new Scanner(System.in).nextInt();
 	System.out.println(one + two + "の合計は"+ (one + two) + "です");
-
+	}
 }
