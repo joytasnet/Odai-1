da08648045d5487daea173d102f3be31b12761db
arukreid <sasaki3y98521j@gmail.com>
2022-04-01 15:15:14 +0900

    Q005 完了

--- sasaki/Q005.java
+++ sasaki/Q005.java
@@ -9,5 +9,10 @@
 import java.util.*;
 public class Q005{
 	public static void main(String[] args){
+		System.out.print("整数を入力>");
+		int num = new Scanner(System.in).nextInt();
+		for(int i=0;i<num;i++){
+			System.out.printf("%d ",i+1);
+		}
 	}
 }
