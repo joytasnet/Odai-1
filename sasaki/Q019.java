6be8ca3b44907fb5d501f4d939fdb138f3a3db0d
sasakiy916 <sasaki3y98521j@gmail.com>
2022-04-02 11:55:30 +0900

    Q019 完了

--- sasaki/Q019.java
+++ sasaki/Q019.java
@@ -9,5 +9,23 @@
 import java.util.*;
 public class Q019{
 	public static void main(String[] args){
+		System.out.print("数値をカンマ区切りで入力>>");
+		String numSt = new Scanner(System.in).nextLine();
+		String[] numSts = numSt.split(",");
+		int[] nums = new int[numSts.length];
+		for(int i=0;i<numSts.length;i++){
+			nums[i] = Integer.parseInt(numSts[i]);
+		}
+		int max = nums[0];
+		int min = nums[0];
+		for(int i=1;i<nums.length;i++){
+			if(max < nums[i]){
+				max = nums[i];
+			}
+			if(min == 0 || min > nums[i]){
+				min = nums[i];
+			}
+		}
+		System.out.printf("最大値は%d,最小値は%d%n",max,min);
 	}
 }
