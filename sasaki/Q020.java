2eed5a4d3e38b26704210d86e7c69c0021c06ca5
sasakiy916 <sasaki3y98521j@gmail.com>
2022-04-02 12:11:10 +0900

    Q020 完了

--- sasaki/Q020.java
+++ sasaki/Q020.java
@@ -12,5 +12,22 @@ Paul
 import java.util.*;
 public class Q020{
 	public static void main(String[] args){
+		System.out.print("名前をカンマ区切りで入力>>");
+		String name = new Scanner(System.in).nextLine();
+		String[] names = name.split(",");
+		for(int i=0;i<names.length-1;i++){
+			for(int j=i+1;j<names.length;j++){
+				if(names[i].length() < names[j].length()){
+					String temp = names[i];
+					names[i] = names[j];
+					names[j] = temp;
+				}
+			}
+			System.out.println(names[i]);
+			if(i == names.length-2){
+				i++;
+				System.out.println(names[i]);
+			}
+		}
 	}
 }
