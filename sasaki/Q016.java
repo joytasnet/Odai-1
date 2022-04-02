e6d9d8af4c2f84ee64fe58c66e8f7450fdb80e5d
sasakiy916 <sasaki3y98521j@gmail.com>
2022-04-02 11:06:17 +0900

    Q016 完了

--- sasaki/Q016.java
+++ sasaki/Q016.java
@@ -8,5 +8,13 @@
 import java.util.*;
 public class Q016{
 	public static void main(String[] args){
+		System.out.print("金額を入力>>");
+		int money = new Scanner(System.in).nextInt();
+		if(money >= 1000){
+			double moneyR = money / 100;
+			System.out.printf("約%,d円です。",Math.round(moneyR)*100);
+		}else{
+			System.out.println("金額は1000円以上で入力してくだい。");
+		}
 	}
 }
