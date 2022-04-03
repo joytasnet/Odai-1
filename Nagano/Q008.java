7ac434c44741ba518154aaf18534fdfff7d56892
yakan <yume_drop@hotmail.com>
2022-04-03 10:23:18 +0900

    Q008追加

--- Nagano/Q008.java
+++ Nagano/Q008.java
@@ -1,14 +1,14 @@
 /*
-【問題008】
-2つの整数を入力すると、その範囲の偶数を昇順で表示する。
-2つの整数が大小逆に入力されても大丈夫なものにする。
+   【問題008】
+   2つの整数を入力すると、その範囲の偶数を昇順で表示する。
+   2つの整数が大小逆に入力されても大丈夫なものにする。
 
-【実行画面】
-整数Aを入力>>20【enterキー】
-整数Bを入力>>10【enterキー】
-10から20の偶数は、
-10 12 14 16 18 20 です
-*/
+   【実行画面】
+   整数Aを入力>>20【enterキー】
+   整数Bを入力>>10【enterキー】
+   10から20の偶数は、
+   10 12 14 16 18 20 です
+   */
 import java.util.*;
 public class Q008{
 	public static void main(String[] args){
@@ -16,20 +16,18 @@ public class Q008{
 		int numA=new Scanner(System.in).nextInt();
 		System.out.print("整数Bを入力>");
 		int numB=new Scanner(System.in).nextInt();
-		for(int i = numA;i<=numB;i++){
-			if(numA<numB){
+		int min =Math.min(numA,numB);
+		int max =Math.max(numA,numB);
+		System.out.println(min +"から"+ max + "の偶数は ");
+		for(int i = min;i<=max;i++){
+			if(min<max){
 				if(i%2==0){
-				System.out.print(i + " ");
+					System.out.print(i + " ");
 				}
 			}				
 		}
-		for(int i = numB;i<=numA;i++){
-			if(numA>numB){
-				if(i%2==0){
-				System.out.print(i + " ");
-				}
-			}				
-		}
+					System.out.print("です");
+
 
 	}
 }
