d938850f1d7980b31af3d235b24a802cfd95eab5
yakan <yume_drop@hotmail.com>
2022-04-04 00:09:00 +0900

    Q021追加

--- Nagano/Q020.java
+++ Nagano/Q020.java
@@ -10,7 +10,17 @@ John
 Paul
 */
 import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 public class Q020{
 	public static void main(String[] args){
+		System.out.print("名前をカンマ区切りで入力>");
+		String name =new Scanner(System.in).next();
+		String[] names =name.split(",");
+		for(int i = 0;i< names.length;i++){
+		}
+		ArrayList<String> list = new ArrayList<>(Arrays.asList(names));
+		list.sort(Comparator.comparingInt(String::length).reversed());
+		System.out.println( list);
 	}
 }
--- Nagano/Q021.java
+++ Nagano/Q021.java
@@ -7,7 +7,24 @@
 38>31>25>4>2
 */
 import java.util.*;
-public class Q020{
+public class Q021{
 	public static void main(String[] args){
+		int[]data =new int[5];
+		for(int i =0;i<data.length;i++){
+			data[i] = new Random().nextInt(20)+20;
+		}
+		System.out.println(Arrays.toString(data));
+		for(int i =0;i<data.length-1;i++){
+			for(int j =i+1;j<data.length;j++){
+				if(data[i]<data[j]){
+					int temp=data[i];
+					data[i]=data[j];
+					data[j]=temp;
+				}
+			}
+		}
+		for (int i =0; i<data.length; i++) {
+			System.out.print(data[i] + ">");
+		}
 	}
 }
