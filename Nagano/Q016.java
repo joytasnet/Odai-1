1afda9a3c8a573d1eefbe971cc733d7c8550c2ce
yakan <yume_drop@hotmail.com>
2022-04-03 15:14:19 +0900

    Q016追加

--- Nagano/Q016.java
+++ Nagano/Q016.java
@@ -8,5 +8,11 @@
 import java.util.*;
 public class Q016{
 	public static void main(String[] args){
+		System.out.print("金額を数字で入力>");
+		double money =new Scanner(System.in).nextInt();
+		money = money * 0.01; 
+		money= Math.floor(money);
+		money=money*100;
+		System.out.printf("約%,d円です",(int)money);
 	}
 }
