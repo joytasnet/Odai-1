3aa8923034c4b01c2036a3ecff3967d9199d9078
cshm <cshm225@gmail.com>
2022-04-05 00:14:48 +0900

    014修正

--- Matsuoka/Q014.java
+++ Matsuoka/Q014.java
@@ -23,18 +23,36 @@ public class Q014{
 	public static void main(String[] args){
 		System.out.print("受験人数(1～9人)を入力してください:");
 		int student=new Scanner(System.in).nextInt();
-		int score=-1,j=0;
-		System.out.printf("------------------------------------------------------------%n    ■ 採点結果 一覧表%n------------------------------------------------------------%n");
-		for(int i=0;i<student;i++){
-			score=new Random().nextInt(50);
-			System.out.printf("受験者%d |",i+1);
-			for(j=0;j<score;j++){
-				System.out.print((j+1)%10==0?"+":"*");
-				if(j==score-1){
-					System.out.printf(" %d点%n",score);
+		int score=-1;
+		for(int i=0;i<student+4;i++){
+			if(i<3||i>=student+3){
+				for(int j=0;j<59;j++){
+					if(i==1){
+						System.out.print("■ 採点結果 一覧表");
+						break;
+					}else if(i==student+4){
+						System.out.print(" ");
+					}else if(i==student+3&&j+2==10){
+						System.out.print("|");
+					}else if(i==student+3&&(j+2)%10==0){
+						System.out.print("+");
+					}else{
+						System.out.print("-");
+					}
 				}
 			}
+			if(i>2&&i<student+3){
+				score=new Random().nextInt(50);
+				System.out.printf("受験者%d |",i-2);
+				for(int j=0;j<score;j++){
+					System.out.print((j+1)%10==0?"+":"*");
+					if(j==score-1){
+						System.out.printf(" %d点",score);
+					}
+				}
+			}
+			System.out.println();
 		}
-		System.out.printf("--------|---------+---------+---------+---------+---------+%n        0        10        20        30        40        50");
+			System.out.println("        0         10        20        30        40        50");
 	}
 }
