229cc826cad38d773183a2be1f2b1fcb22c4f04b
ltbur <ltburtonst@gmail.com>
2022-04-05 06:58:38 +0900

    20,21追加

--- Abe/Q020.java
+++ Abe/Q020.java
@@ -12,5 +12,23 @@ Paul
 import java.util.*;
 public class Q020{
 	public static void main(String[] args){
+		System.out.print("名前をカンマ区切りで入力>>");
+		String namesStr = new Scanner(System.in).nextLine();
+
+		String[] names = namesStr.split(",");
+
+		for(int i = 0; i < names.length - 1; i++) {
+			int longInx = i;
+			for(int j = i + 1; j < names.length; j++)
+				if(names[j].length() >= names[longInx].length())
+					longInx = j;
+			String tmp = names[longInx];
+			names[longInx] = names[i];
+			names[i] = tmp;
+		}
+
+		for(int i = 0; i < names.length; i++){
+			System.out.println(names[i]);
+		}
 	}
 }
--- Abe/Q021.java
+++ Abe/Q021.java
@@ -7,7 +7,31 @@
 38>31>25>4>2
 */
 import java.util.*;
-public class Q020{
+public class Q021{
 	public static void main(String[] args){
+		int[] nums = new int[5];
+		for(int i = 0; i < nums.length; i++)
+			nums[i] = new Random().nextInt(21) + 20;
+
+		System.out.print("[" + nums[0]);
+		for(int i = 1; i < nums.length; i++)
+			System.out.print("," + nums[i]);
+		System.out.println("]");
+
+		for(int i = 0; i < nums.length - 1; i++) {
+			int maxInx = i;
+			for(int j = i + 1; j < nums.length; j++)
+				if(nums[j] > nums[maxInx])
+					maxInx = j;
+			int tmp = nums[maxInx];
+			nums[maxInx] = nums[i];
+			nums[i] = tmp;
+		}
+
+		System.out.print(nums[0] + "");
+		for(int i = 1; i < nums.length; i++){
+			System.out.print(">" + nums[i]);
+		}
+		System.out.println();
 	}
 }
