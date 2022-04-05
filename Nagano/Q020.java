08d3855d438eea7b76e4bb2adba30a6a70eca0d2
yakan <yume_drop@hotmail.com>
2022-04-05 12:20:18 +0900

    Odai追加

--- Nagano/Q020.java
+++ Nagano/Q020.java
@@ -10,17 +10,20 @@ John
 Paul
 */
 import java.util.*;
-import java.util.Arrays;
-import java.util.Comparator;
 public class Q020{
 	public static void main(String[] args){
 		System.out.print("名前をカンマ区切りで入力>");
 		String name =new Scanner(System.in).next();
 		String[] names =name.split(",");
-		for(int i = 0;i< names.length;i++){
+		for(int i =0;i<names.length;i++){
+			for(int j =i+1;j<names.length;j++){
+				if(names[i].length()<names[j].length()){
+					String temp=names[i];
+					names[i]=names[j];
+					names[j]=temp;
+				}
+			}
+			System.out.println(names[i]);
 		}
-		ArrayList<String> list = new ArrayList<>(Arrays.asList(names));
-		list.sort(Comparator.comparingInt(String::length).reversed());
-		System.out.println( list);
 	}
 }
