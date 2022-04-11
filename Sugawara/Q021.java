ee6769e798c8931d80c2eed982729678011a226a
pommebarque-megumi <cache2gecko@gmail.com>
2022-04-11 23:24:23 +0900

    Q021忘れていたのでやりました。

--- Sugawara/Q021.java
+++ Sugawara/Q021.java
@@ -7,7 +7,25 @@
 38>31>25>4>2
 */
 import java.util.*;
-public class Q020{
+public class Q021{
 	public static void main(String[] args){
+		int[] nums = new int[5];
+		for(int i=0 ; i<nums.length ; i++){
+			nums[i] = new Random().nextInt(21)+20;
+		}
+		System.out.println(Arrays.toString(nums));
+		for(int i=0 ; i<nums.length-1 ; i++){
+			for(int j=i+1 ; j<nums.length ;j++){
+				if(nums[i]<nums[j]){
+					int temp = nums[j];
+					nums[j] = nums[i];
+					nums[i] = temp;
+				}
+			}
+		}
+		for(int num : nums){
+			System.out.printf("%d>",num);
+		}
+		System.out.println();
 	}
 }
